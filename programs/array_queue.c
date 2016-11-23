379d627378b63e6b36c7a5d7bdd2f787e419b5e4
Kostas Kokkoros <mavroprovato@gmail.com>
2016-11-23 12:47:02 +0200

    Test program for array queue

--- programs/array_queue.c
+++ programs/array_queue.c
@@ -1,11 +1,11 @@
 /**
- * Test program for the array stack implementation.
+ * Test program for the array queue implementation.
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "array_stack.h"
+#include "array_queue.h"
 
 int main(int argc, char **argv) {
     // Open file
@@ -20,12 +20,12 @@ int main(int argc, char **argv) {
         fp = stdin;
     }
 
-    // Initialize the stack
+    // Initialize the queue
     char *line = NULL;
     int return_val = 0;
-    ArrayStack stack;
-    if (!as_init(&stack)) {
-        fprintf(stderr, "Could not create the stack data structure.\n");
+    ArrayQueue queue;
+    if (!aq_init(&queue)) {
+        fprintf(stderr, "Could not create the queue data structure.\n");
         return_val = 1;
         goto cleanup;
     }
@@ -34,12 +34,12 @@ int main(int argc, char **argv) {
     size_t len = 0;
     ssize_t read;
     while ((read = getline(&line, &len, fp)) != -1) {
-        // Perform the stack operation based on the command
+        // Perform the queue operation based on the command
         if (strncmp(line, "is_empty", strlen("is_empty")) == 0) {
-            printf("%s\n", as_is_empty(&stack) ? "empty" : "not empty");
+            printf("%s\n", aq_is_empty(&queue) ? "empty" : "not empty");
         } else if (strncmp(line, "size", strlen("size")) == 0) {
-            printf("%zu\n", as_size(&stack));
-        } else if (strncmp(line, "push", strlen("push")) == 0) {
+            printf("%zu\n", aq_size(&queue));
+        } else if (strncmp(line, "enqueue", strlen("enqueue")) == 0) {
             // Find the command
             char *space_idx = strchr(line, ' ');
             if (!space_idx) {
@@ -47,31 +47,31 @@ int main(int argc, char **argv) {
                 return_val = 1;
                 goto cleanup;
             }
-            // Find and push the string
+            // Find and enqueue the string
             char *s = strndup(space_idx + 1, strlen(space_idx) - 2);
             if (!s) {
-                fprintf(stderr, "Cannot push to stack.\n");
+                fprintf(stderr, "Cannot enqueue element.\n");
                 return_val = 1;
                 goto cleanup;
             }
-            if (!as_push(&stack, s)) {
-                fprintf(stderr, "Cannot push to stack.\n");
+            if (!aq_enqueue(&queue, s)) {
+                fprintf(stderr, "Cannot enqueue element.\n");
                 return_val = 1;
                 goto cleanup;
             }
-        } else if (strncmp(line, "pop", strlen("pop")) == 0) {
-            char *s = as_pop(&stack);
+        } else if (strncmp(line, "dequeue", strlen("dequeue")) == 0) {
+            char *s = aq_dequeue(&queue);
             if (!s) {
-                fprintf(stderr, "Cannot pop from stack.\n");
+                fprintf(stderr, "Cannot dequeue element.\n");
                 return_val = 1;
                 goto cleanup;
             }
             printf("%s\n", s);
             free(s);
         } else if (strncmp(line, "peek", strlen("peek")) == 0) {
-            char *s = as_peek(&stack);
+            char *s = aq_peek(&queue);
             if (!s) {
-                fprintf(stderr, "Cannot peek stack.\n");
+                fprintf(stderr, "Cannot peek queue.\n");
                 return_val = 1;
                 goto cleanup;
             }
@@ -85,7 +85,7 @@ int main(int argc, char **argv) {
 
     // Clean up
 cleanup:
-    as_destroy(&stack);
+    aq_destroy(&queue);
     free(line);
     fclose(fp);
 
