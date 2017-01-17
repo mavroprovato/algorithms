9df3ff202fc518f0f1ae138828c1f267a06728ad
Kostas Kokkoros <mavroprovato@gmail.com>
2017-01-17 18:01:10 +0200

    Update the queue test program to also test the linked list implementation

--- programs/queue.c
+++ programs/queue.c
@@ -1,7 +1,10 @@
 /**
- * Test program for the array queue implementation.
+ * Test program for the queue implementations.
  */
 #include "array_queue.h"
+#include "linked_queue.h"
+
+#include <getopt.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,23 +23,48 @@ void print_element(void *item, void *data) {
 }
 
 int main(int argc, char **argv) {
-    // Open file
-    FILE * fp;
-    if (argc == 2) {
-        fp = fopen(argv[1], "r");
+    // Parse the command line arguments
+    static struct option long_options[] = {
+        {"linked", no_argument, 0, 'l'},
+        {0, 0, 0, 0}
+    };
+    int option_index = 0;
+    int c = -1;
+    bool linked = false;
+    while ((c = getopt_long(argc, argv, "l", long_options, &option_index)) != -1) {
+        switch (c) {
+            case 'l':
+                linked = true;
+                break;
+            default:
+                return 0;
+        }
+    }
+
+    // Check if a file was provided to be opened
+    FILE *fp;
+    if (optind < argc) {
+        fp = fopen(argv[optind], "r");
         if (!fp) {
-            fprintf(stderr, "Could not open file: %s.\n", argv[1]);
+            fprintf(stderr, "Could not open file: %s.\n", argv[optind]);
             return 1;
         }
     } else {
         fp = stdin;
     }
 
-    // Initialize the queue
     char *line = NULL;
     int return_val = 0;
-    ArrayQueue queue;
-    if (!aq_init(&queue)) {
+
+    // Initialize the queues
+    ArrayQueue aq;
+    if (!aq_init(&aq)) {
+        fprintf(stderr, "Could not create the queue data structure.\n");
+        return_val = 1;
+        goto cleanup;
+    }
+    LinkedQueue lq;
+    if (!lq_init(&lq)) {
         fprintf(stderr, "Could not create the queue data structure.\n");
         return_val = 1;
         goto cleanup;
@@ -48,9 +76,11 @@ int main(int argc, char **argv) {
     while ((read = getline(&line, &len, fp)) != -1) {
         // Perform the queue operation based on the command
         if (strncmp(line, "is_empty", strlen("is_empty")) == 0) {
-            printf("%s\n", aq_is_empty(&queue) ? "empty" : "not empty");
+            bool empty = linked ? lq_is_empty(&lq) : aq_is_empty(&aq);
+            printf("%s\n", empty ? "empty" : "not empty");
         } else if (strncmp(line, "size", strlen("size")) == 0) {
-            printf("%zu\n", aq_size(&queue));
+            size_t size = linked ? lq_size(&lq) : aq_size(&aq);
+            printf("%zu\n", size);
         } else if (strncmp(line, "enqueue ", strlen("enqueue ")) == 0) {
             // Find the string
             char *space_idx = strchr(line, ' ');
@@ -65,13 +95,21 @@ int main(int argc, char **argv) {
                 return_val = 1;
                 goto cleanup;
             }
-            if (!aq_enqueue(&queue, s)) {
-                fprintf(stderr, "Cannot enqueue element.\n");
-                return_val = 1;
-                goto cleanup;
+            if (linked) {
+                if (!lq_enqueue(&lq, s)) {
+                    fprintf(stderr, "Cannot enqueue element.\n");
+                    return_val = 1;
+                    goto cleanup;
+                }
+            } else {
+                if (!aq_enqueue(&aq, s)) {
+                    fprintf(stderr, "Cannot enqueue element.\n");
+                    return_val = 1;
+                    goto cleanup;
+                }
             }
         } else if (strncmp(line, "dequeue", strlen("dequeue")) == 0) {
-            char *s = aq_dequeue(&queue);
+            char *s = linked ? lq_dequeue(&lq) : aq_dequeue(&aq);
             if (!s) {
                 fprintf(stderr, "Cannot dequeue element.\n");
                 return_val = 1;
@@ -80,7 +118,7 @@ int main(int argc, char **argv) {
             printf("%s\n", s);
             free(s);
         } else if (strncmp(line, "peek", strlen("peek")) == 0) {
-            char *s = aq_peek(&queue);
+            char *s = linked ? lq_peek(&lq) : aq_peek(&aq);
             if (!s) {
                 fprintf(stderr, "Cannot peek queue.\n");
                 return_val = 1;
@@ -89,7 +127,11 @@ int main(int argc, char **argv) {
             printf("%s\n", s);
         } else if (strncmp(line, "print", strlen("print")) == 0) {
             // Print all elements
-            aq_foreach(&queue, print_element, NULL);
+            if (linked) {
+                lq_foreach(&lq, print_element, NULL);
+            } else {
+                aq_foreach(&aq, print_element, NULL);
+            }
             puts("");
         } else {
             fprintf(stderr, "Invalid command: %.*s.\n", (int) read - 1, line);
@@ -99,7 +141,14 @@ int main(int argc, char **argv) {
 
     // Clean up
 cleanup:
-    aq_destroy(&queue);
+    while (!aq_is_empty(&aq)) {
+        free(aq_dequeue(&aq));
+    }
+    aq_destroy(&aq);
+    while (!lq_is_empty(&lq)) {
+        free(lq_dequeue(&lq));
+    }
+    lq_destroy(&lq);
     free(line);
     fclose(fp);
 
