6a5f12a92705ce0930995e3a04cf5aca56ac87e3
Kostas Kokkoros <mavroprovato@gmail.com>
2017-02-09 03:43:53 +0200

    Stack based on fixed sized array

---
+++ include/fastack.h
@@ -0,0 +1,90 @@
+#ifndef _FA_STACK_H
+#define _FA_STACK_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#include "common.h"
+
+/**
+ * A stack data structure, backed by an array.
+ */
+typedef struct {
+    /** The stack elements. */
+    void **items;
+    /** The stack size. */
+    size_t size;
+    /** The current capacity of the stack. */
+    size_t capacity;
+} FAStack;
+
+/**
+ * Initialize the fixed array stack data structure.
+ *
+ * @param fas Pointer to the fixed array stack data structure.
+ * @return true if the data structure was initialized successfully, false
+ * otherwise.
+ */
+bool fas_init(FAStack *fas);
+
+/**
+ * Frees resources associated with the fixed array stack data structure.
+ *
+ * @param fas Pointer to the fixed array stack data structure to be freed.
+ */
+void fas_destroy(FAStack *fas);
+
+/**
+ * Check if the fixed array stack contains any elements.
+ *
+ * @param fas Pointer to the fixed array stack data structure.
+ * @return true if the stack contains elements, false otherwise.
+ */
+bool fas_is_empty(FAStack *fas);
+
+/**
+ * Return the size of the fixed array stack.
+ *
+ * @param fas Pointer to the fixed array stack data structure.
+ * @return The size of the fixed array stack.
+ */
+size_t fas_size(FAStack *fas);
+
+/**
+ * Add an element to the fixed array stack. Note that NULL elements cannot be
+ * added to the stack.
+ *
+ * @param fas Pointer to the fixed array stack data structure.
+ * @return true if the element was pushed successfully, false otherwise.
+ */
+bool fas_push(FAStack *fas, void *item);
+
+/**
+ * Removes and returns the last element that was added to the fixed array stack.
+ *
+ * @param fas Pointer to the fixed array stack data structure.
+ * @return The last element that was added to the fixed array stack, or NULL if
+ * the stack was empty.
+ */
+void *fas_pop(FAStack *fas);
+
+/**
+ * Returns the last element that was added to the fixed array stack.
+ *
+ * @param fas Pointer to the fixed array stack data structure.
+ * @return The last element that was added to the fixed array stack, or NULL if
+ * the stack was empty.
+ */
+void *fas_peek(FAStack *fas);
+
+/**
+ * Traverse all the items of the fixed array stack and call a function for each
+ * of them.
+ *
+ * @param fas Pointer to the fixed array stack data structure.
+ * @param iterator_func Pointer to function that is called for every item.
+ * @param data Pointer to the custom user data. Can be NULL.
+ */
+void fas_foreach(FAStack *fas, ITERATOR_FUNC iterator_func, void *data);
+
+#endif // _FA_STACK_H
---
+++ src/fastack.c
@@ -0,0 +1,67 @@
+#include "fastack.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+bool fas_init(FAStack *fas) {
+    fas->capacity = 2;
+    fas->items = malloc(fas->capacity * sizeof(void *));
+    if (!fas->items) {
+        return false;
+    }
+    fas->size = 0;
+
+    return true;
+}
+
+void fas_destroy(FAStack *fas) {
+    // Free the underlying array
+    free(fas->items);
+    fas->items = NULL;
+}
+
+bool fas_is_empty(FAStack *fas) {
+    return fas->size == 0;
+}
+
+size_t fas_size(FAStack *fas) {
+    return fas->size;
+}
+
+bool fas_push(FAStack *fas, void *item) {
+    if (fas->size == fas->capacity) {
+        // Overflow
+        return false;
+    }
+    fas->items[fas->size++] = item;
+
+    return true;
+}
+
+void *fas_pop(FAStack *fas) {
+    if (fas->size == 0) {
+        // The array is empty
+        return NULL;
+    }
+
+    void *item = fas->items[fas->size - 1];
+    fas->items[fas->size - 1] = NULL;
+    fas->size--;
+
+    return item;
+}
+
+void *fas_peek(FAStack *fas) {
+    if (fas->size == 0) {
+        // The array is empty
+        return NULL;
+    }
+
+    return fas->items[fas->size - 1];
+}
+
+void fas_foreach(FAStack *fas, ITERATOR_FUNC iterator_func, void *data) {
+    for (size_t i = fas->size; i-- > 0; ) {
+        iterator_func(fas->items[i], data);
+    }
+}
